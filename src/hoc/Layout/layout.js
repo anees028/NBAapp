b06b4bda11b5b8185bde0192fa84016b582f68f6
anees028 <rehmananees028@gmail.com>
2021-02-13 12:43:07 +0500

    NBA First iteration Upload on Github

---
+++ src/hoc/Layout/layout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+//importing Layout Style..
+import './layout.css';
+
+class Layout extends Component {
+
+    state={
+        
+    }
+
+    render() {
+        return (
+            <div>
+                Header
+                {this.props.children}
+                Footer
+            </div>
+        )
+    }
+}
+
+export default Layout;
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+//Importing Components
+import Home from './components/Home/home';
+import Layout from './hoc/Layout/layout';
+
+class Routes extends Component {
+    render() {
+        return (
+            <Layout>       
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </Layout>
+
+        )
+    }
+}
+
+export default Routes;
