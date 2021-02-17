a01f0f62f6b8e42bacdba7a2dbd5991f62419d80
anees028 <rehmananees028@gmail.com>
2021-02-18 03:23:54 +0500

    6th Iteration Done with Slider Styling except Slider Images

---
+++ src/components/widgets/NewsSlider/slider.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import SliderTemplates from './slider_templates'
+
+class NewsSlider extends Component {
+
+    state={
+        news:[]
+    }
+
+    componentWillMount(){
+        axios.get(`http://localhost:3004/articles?_start=0&_end=3`)
+        .then( response => {
+            this.setState({
+                news:response.data
+            })
+        })
+    }
+
+    render() {
+        console.log(this.state.news) 
+        return (
+            <div>
+                <SliderTemplates data={this.state.news} type="featured" />
+            </div>
+        )
+    }
+}
+
+
+export default NewsSlider;
---
+++ src/components/widgets/NewsSlider/slider_templates.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Slick from 'react-slick';
+import styles from './slider.module.css';
+
+const SliderTemplates = (props) => {
+    //importing picture from PUBLIC_URL..... 
+    let pic = process.env.PUBLIC_URL;
+
+    let template = null
+    const settings={
+        dots:true,
+        arrows:false,
+        infinite:true,
+        speed:500,
+        slidesToShow:1,
+        slidesToScroll:1,
+    }
+
+    switch(props.type){
+        case("featured"):
+            template = props.data.map((item , i) => {
+                return(
+                    <div key={i}>
+                        <div className={styles.featured_item}>
+                            <div className={styles.featured_image} 
+                                style={{
+                                    background: `url(../images/articles/${item.image})` 
+                                }}
+                            >
+                                <Link to={`/articles/${item.id}`} >
+                                    <div className={styles.featured_caption}>
+                                        {item.title}
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+
+            break;
+        default:
+            template = null;
+    }
+
+    return (
+        <Slick {...settings} >
+            {template}
+        </Slick>
+    );
+};
+
+export default SliderTemplates;
