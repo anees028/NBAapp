23640623cf165b070bd43e843f43dae4832ac834
anees028 <rehmananees028@gmail.com>
2021-03-03 14:56:58 +0500

    13th Iterartion:: The news page is completed with rendering the news ...

---
+++ src/components/Articles/News/Main/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import NewsSlider from '../../../widgets/NewsSlider/slider';
+import NewsList from '../../../widgets/NewsList/newsList';
+
+const NewsMain = () => {
+    return (
+        <div>
+            <NewsSlider
+                type="featured"
+                start={0}
+                amount={3}
+                settings={{
+                    dots:false
+                }}
+            />
+            <NewsList 
+                type="cardMain"
+                start={3}
+                amount={3}
+                loadMore={true}
+            />
+        </div>
+    );
+};
+
+export default NewsMain;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import {Route, Switch} from 'react-router-dom';
 //Importing Components
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
+import NewsMain from './components/Articles/News/Main/index';
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
 
@@ -12,7 +13,8 @@ class Routes extends Component {
         return (
             <Layout>       
                 <Switch>
-                    <Route path="/" exact component={Home} />
+                    <Route path="/" exact component={Home} /> 
+                    <Route path="/news" exact component={NewsMain} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
                     <Route path="/videos/:id" exact component={VideoArticle} /> 
                 </Switch>
